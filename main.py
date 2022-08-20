9956197ef44b683a3c56976281e25ad2c939cec7
AvetisyanGagik <105722046+AvetisyanGagik@users.noreply.github.com>
2022-08-20 16:46:41 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# text = '''Melqonyan Areg
+#           August 18, 1976
+#           46 years old
+
+# '''
+# new_text = text.split()
+# print(new_text)
+# # print(new_text[0] + ' ' + new_text[1])
+
